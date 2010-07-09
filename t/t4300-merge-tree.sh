907183075c79b58d22e22a22ebdeaffdc087a465
Will Palmer <wmpalmer@gmail.com>
2010-07-10 00:53:10 +0100

    add basic tests for merge-tree
    merge-tree had no test cases, so here we add some very basic tests for
    it, including one known-breakage.

---
+++ t/t4300-merge-tree.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+#
+# Copyright (c) 2010 Will Palmer
+#
+
+test_description='git merge-tree'
+. ./test-lib.sh
+
+test_expect_success setup '
+	test_commit "initial"
+'
+
+test_expect_success 'both added same' '
+	git reset --hard initial
+	test_commit "same-A" "ONE" "AAA" 
+
+	git reset --hard initial
+	test_commit "same-B" "ONE" "AAA"
+
+	git merge-tree initial same-A same-B
+'
+
+test_expect_success 'both added conflict' '
+	git reset --hard initial
+	test_commit "diff-A" "ONE" "AAA" 
+
+	git reset --hard initial
+	test_commit "diff-B" "ONE" "BBB"
+
+	git merge-tree initial diff-A diff-B
+'
+
+test_expect_failure 'nothing similar' '
+	git reset --hard initial
+	test_commit "no-common-A" "ONE" "AAA" 
+
+	git reset --hard initial
+	test_commit "no-common-B" "TWO" "BBB"
+
+	git merge-tree initial no-common-A no-common-B
+'
+
+test_done
